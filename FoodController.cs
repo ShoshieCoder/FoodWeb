0569855a21894647d03b30cf780b2cbea9c80643
ShoshieCoder <46136018+ShoshieCoder@users.noreply.github.com>
2019-10-01 15:13:44 +0200

    Add files via upload

---
+++ FoodController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FoodWeb.Controllers
+{
+    public class FoodController : ApiController
+    {
+            
+        public FoodDAO f = new FoodDAO();
+        public HttpResponseMessage Get()
+        {
+            List<Food> foods = f.GetAllFoods();
+            HttpResponseMessage msg = Request.CreateResponse(HttpStatusCode.OK, foods);
+            return msg;
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            Food food = f.GetFoodById(id);
+            return Request.CreateResponse(HttpStatusCode.OK, food);
+        }
+
+        public HttpResponseMessage Post([FromBody]Food value)
+        {
+            f.AddFood(value);
+            return Request.CreateResponse(HttpStatusCode.Created, value);
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]Food value)
+        {
+            Food food = f.UpdateFood(id, value);
+            return Request.CreateResponse(HttpStatusCode.Accepted, food);
+        }
+
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            Food food = f.DeleteFood(id);
+            return Request.CreateResponse(HttpStatusCode.OK, food);
+        }
+
+        [Route("api/foods/byname/{name}")]
+        [HttpGet]
+        public HttpResponseMessage GetByName(string name)
+        {
+            List<Food> foods = f.GetAllFoodsByName(name);
+            return Request.CreateResponse(HttpStatusCode.OK, foods);
+        }
+
+        [Route("api/foods/bymincal/{mincalories}")]
+        [HttpGet]
+        public HttpResponseMessage GetByMinCalories(int mincalories)
+        {
+            List<Food> foods = f.GetAllFoodsByMinCalories(mincalories);
+            return Request.CreateResponse(HttpStatusCode.OK, foods);
+        }
+
+        [Route("api/foods/search")]
+        [HttpGet]
+        public HttpResponseMessage GetByFilter(string name, int maxcal, int mincal, string ingridients, int grade)
+        {
+            List<Food> foods = f.GetByFilter(name, mincal, maxcal, ingridients, grade);
+            return Request.CreateResponse(HttpStatusCode.OK, foods);
+        }
+    }
+}
+
---
+++ FoodDAO.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodWeb
+{
+    public class FoodDAO
+    {
+        public List<Food> GetAllFoods()
+        {
+            using (FoodDBEntities foodEntites = new FoodDBEntities())
+            {
+                return foodEntites.Foods.ToList();
+            }
+        }
+
+        public Food GetFoodById(int id)
+        {
+            using (FoodDBEntities foodEntites = new FoodDBEntities())
+            {
+                return foodEntites.Foods.FirstOrDefault(f => f.ID == id);
+            }
+        }
+
+        public void AddFood(Food food)
+        {
+            using (FoodDBEntities foodEntites = new FoodDBEntities())
+            {
+                foodEntites.Foods.Add(food);
+                foodEntites.SaveChanges();
+            }
+
+        }
+
+        public Food UpdateFood(int id, Food food)
+        {
+            Food foodForUpdate = new Food();
+            using (FoodDBEntities foodEntites = new FoodDBEntities())
+            {
+                foodForUpdate = foodEntites.Foods.FirstOrDefault(f => f.ID == id);
+                foodForUpdate.Name = food.Name;
+                foodForUpdate.Calories = food.Calories;
+                foodForUpdate.Ingridients = food.Ingridients;
+                foodForUpdate.Grade = food.Grade;
+                foodEntites.SaveChanges();
+            }
+            return foodForUpdate;
+        }
+
+        public Food DeleteFood(int id)
+        {
+            Food foodForRemove;
+            using (FoodDBEntities foodEntites = new FoodDBEntities())
+            {
+                foodForRemove = foodEntites.Foods.FirstOrDefault(f => f.ID == id);
+                foodEntites.Foods.Remove(foodForRemove);
+                foodEntites.SaveChanges();
+            }
+            return foodForRemove;
+        }
+
+        public List<Food> GetAllFoodsByName(string name)
+        {
+            using (FoodDBEntities foodEntites = new FoodDBEntities())
+            {
+                return foodEntites.Foods.Where(f => f.Name.ToUpper().Contains(name.ToUpper())).ToList();
+            }
+        }
+
+        public List<Food> GetAllFoodsByMinCalories(int Calories)
+        {
+            using (FoodDBEntities foodEntites = new FoodDBEntities())
+            {
+                return foodEntites.Foods.Where(f => f.Calories > Calories).ToList();
+            }
+        }
+
+        public List<Food> GetByFilter(string name, int maxcal, int mincal, string ingridients, int grade)
+        {
+            using (FoodDBEntities foodDBEntities = new FoodDBEntities())
+            {
+                if (name == null && mincal.Equals(null) && maxcal.Equals(null) && ingridients == null && !grade.Equals(null))
+                    return foodDBEntities.Foods.Where(m => m.Grade == grade).ToList();
+
+                if (name == null && mincal.Equals(null) && maxcal.Equals(null) && !(ingridients.Equals(null)) && grade.Equals(null))
+                    return foodDBEntities.Foods.Where(m => m.Ingridients.ToUpper().Contains(ingridients.ToUpper())).ToList();
+
+                if (name == null && mincal > 0 && maxcal.Equals(null) && ingridients == null && grade.Equals(null))
+                    return foodDBEntities.Foods.Where(m => m.Calories >= mincal).ToList();
+
+                if (name == null && mincal.Equals(null) && maxcal > 0 && ingridients == null && grade.Equals(null))
+                    return foodDBEntities.Foods.Where(m => m.Calories <= maxcal).ToList();
+
+                if (!(name.Equals(null)) && mincal.Equals(null) && maxcal.Equals(null) && ingridients == null && grade.Equals(null))
+                    return foodDBEntities.Foods.Where(m => m.Name.ToUpper().Contains(name.ToUpper())).ToList();
+
+                if (name == null && mincal > 0 && maxcal > 0 && ingridients == null && grade.Equals(null))
+                    return foodDBEntities.Foods.Where(m => m.Calories >= mincal && m.Calories <= maxcal).ToList();
+
+                return foodDBEntities.Foods.Where(m => m.Name.ToUpper().Contains(name.ToUpper()) || (!(ingridients.Equals(null)) && m.Ingridients.ToUpper().Contains(ingridients.ToUpper())) || (!grade.Equals(null)) && m.Grade == grade || (mincal > 0 && maxcal.Equals(null) && m.Calories >= mincal) || (mincal.Equals(null) && maxcal > 0 && m.Calories <= maxcal) || (mincal > 0 && maxcal > 0 && m.Calories >= mincal && m.Calories <= maxcal)).ToList();
+
+            }
+
+        }
+    }
+}
